489ff3ca4276cf7eb80ebf5d68cbd0c97a3424a8
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-10-16 21:08:48 +0300

    Add polling. Make buffer size configurable. Format with prettier

--- index.js
+++ index.js
@@ -1,100 +1,134 @@
-const Kafka = require('node-rdkafka')
-const zk = require('node-zookeeper-client-async')
+const Kafka = require("node-rdkafka");
+const zk = require("node-zookeeper-client-async");
 
-const ZOOKEEPER_URL = process.env.ZOOKEEPER_URL || "localhost:2181"
-const zookeeperClient = zk.createAsyncClient(ZOOKEEPER_URL)
+const ZOOKEEPER_URL = process.env.ZOOKEEPER_URL || "localhost:2181";
+const zookeeperClient = zk.createAsyncClient(ZOOKEEPER_URL);
 
-const KAFKA_COMPRESSION_CODEC = process.env.KAFKA_COMPRESSION_CODEC || "lz4"
-const KAFKA_URL = process.env.KAFKA_URL || "localhost:9092"
-const KAFKA_FLUSH_TIMEOUT = parseInt(process.env.KAFKA_FLUSH_TIMEOUT || "5000")
+const KAFKA_COMPRESSION_CODEC = process.env.KAFKA_COMPRESSION_CODEC || "lz4";
+const KAFKA_URL = process.env.KAFKA_URL || "localhost:9092";
+const KAFKA_FLUSH_TIMEOUT = parseInt(process.env.KAFKA_FLUSH_TIMEOUT || "5000");
+const BUFFERING_MAX_MESSAGES = parseInt(
+  process.env.BUFFERING_MAX_MESSAGES || "150000"
+);
+const FORMAT_HEADER = "format=json;";
 
-const FORMAT_HEADER = 'format=json;'
-
-process.on('unhandledRejection', (reason, p) => {
+process.on("unhandledRejection", (reason, p) => {
   // Otherwise unhandled promises are not possible to trace with the information logged
-  console.error('Unhandled Rejection at: ', p, 'reason:', reason, 'error stack:', reason.stack)
-  process.exit(1)
-})
+  console.error(
+    "Unhandled Rejection at: ",
+    p,
+    "reason:",
+    reason,
+    "error stack:",
+    reason.stack
+  );
+  process.exit(1);
+});
 
 exports.Exporter = class {
   constructor(exporter_name) {
-    this.exporter_name = exporter_name
+    this.exporter_name = exporter_name;
     this.producer = new Kafka.Producer({
-      'metadata.broker.list': KAFKA_URL,
-      'client.id': this.exporter_name,
-      'compression.codec': KAFKA_COMPRESSION_CODEC
-    })
+      "metadata.broker.list": KAFKA_URL,
+      "client.id": this.exporter_name,
+      "compression.codec": KAFKA_COMPRESSION_CODEC,
+      "queue.buffering.max.messages": BUFFERING_MAX_MESSAGES
+    });
   }
 
   get topic_name() {
-    return process.env.KAFKA_TOPIC || this.exporter_name.replace("-exporter", "").replace("-", "_")
+    return (
+      process.env.KAFKA_TOPIC ||
+      this.exporter_name.replace("-exporter", "").replace("-", "_")
+    );
   }
 
   get zookeeperPositionNode() {
-    return `/${this.exporter_name}/${this.topic_name}/block-number`
+    return `/${this.exporter_name}/${this.topic_name}/block-number`;
   }
 
   async connect() {
-    console.log(`Connecting to zookeeper host ${ZOOKEEPER_URL}`)
-    await zookeeperClient.connectAsync()
+    console.log(`Connecting to zookeeper host ${ZOOKEEPER_URL}`);
+    await zookeeperClient.connectAsync();
 
-    console.info(`Connecting to kafka host ${KAFKA_URL}`)
-    this.producer.connect()
+    console.info(`Connecting to kafka host ${KAFKA_URL}`);
+    this.producer.connect();
     return new Promise((resolve, reject) => {
-      this.producer.on("ready", resolve)
-      this.producer.on("event.error", reject)
-    })
+      this.producer.on("ready", _arg => {
+        this.producer.setPollInterval(3000);
+
+        resolve;
+      });
+      this.producer.on("event.error", reject);
+      this.producer.on("delivery-report", function(err, report) {
+        console.log("delivery-report: " + JSON.stringify(report));
+        counter++;
+      });
+    });
   }
 
   async getLastPosition() {
     if (await zookeeperClient.existsAsync(this.zookeeperPositionNode)) {
-      const previousBlockNumber = await zookeeperClient.getDataAsync(this.zookeeperPositionNode)
+      const previousBlockNumber = await zookeeperClient.getDataAsync(
+        this.zookeeperPositionNode
+      );
 
       try {
         if (Buffer.isBuffer(previousBlockNumber && previousBlockNumber.data)) {
-          const value = previousBlockNumber.data.toString('utf8')
+          const value = previousBlockNumber.data.toString("utf8");
 
           if (value.startsWith(FORMAT_HEADER)) {
-            return JSON.parse(value.replace(FORMAT_HEADER, ''))
+            return JSON.parse(value.replace(FORMAT_HEADER, ""));
           } else {
-            return previousBlockNumber.data.readUInt32BE(0)
+            return previousBlockNumber.data.readUInt32BE(0);
           }
         }
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     }
 
-    return null
+    return null;
   }
 
   async savePosition(position) {
-    if (typeof position !== 'undefined') {
-      const newNodeValue = Buffer.from(FORMAT_HEADER + JSON.stringify(position), 'utf-8')
+    if (typeof position !== "undefined") {
+      const newNodeValue = Buffer.from(
+        FORMAT_HEADER + JSON.stringify(position),
+        "utf-8"
+      );
 
       if (await zookeeperClient.existsAsync(this.zookeeperPositionNode)) {
-        return zookeeperClient.setDataAsync(this.zookeeperPositionNode, newNodeValue)
+        return zookeeperClient.setDataAsync(
+          this.zookeeperPositionNode,
+          newNodeValue
+        );
       } else {
-        return zookeeperClient.mkdirpAsync(this.zookeeperPositionNode, newNodeValue)
+        return zookeeperClient.mkdirpAsync(
+          this.zookeeperPositionNode,
+          newNodeValue
+        );
       }
     }
   }
 
   async sendData(events) {
     if (events.constructor !== Array) {
-      events = [events]
+      events = [events];
     }
 
-    events = events.map((event) => typeof(event) === "object" ? JSON.stringify(event) : event)
-    events.forEach((event) => {
-      this.producer.produce(this.topic_name, null, Buffer.from(event))
-    })
+    events = events.map(
+      event => (typeof event === "object" ? JSON.stringify(event) : event)
+    );
+    events.forEach(event => {
+      this.producer.produce(this.topic_name, null, Buffer.from(event));
+    });
 
     return new Promise((resolve, reject) =>
       this.producer.flush(KAFKA_FLUSH_TIMEOUT, (err, result) => {
-        if (err) return reject(err)
-        resolve(result)
+        if (err) return reject(err);
+        resolve(result);
       })
-    )
+    );
   }
-}
+};
