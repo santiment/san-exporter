f0ecf8d24b0bcc2dc3800cc06e411716cbfc4cd4
Yordan Pavlov <y.d.pavlov@gmail.com>
2021-04-21 18:07:06 +0300

    Style improvements in examples.

--- examples/send_dates.js
+++ examples/send_dates.js
@@ -3,19 +3,16 @@ const { Exporter } = require('../index')
 const exporter = new Exporter(pkg.name)
 
 async function pushData(num_iteration) {
-  const timestamp = Math.floor(new Date() / 1000)
-
   let lastPosition = await exporter.getLastPosition()
   console.log(`Last position: ${JSON.stringify(lastPosition)}`)
-  let key = 1
-  if(lastPosition) {
-    key = lastPosition.key + 1
-  }
+  const key = lastPosition ? lastPosition.key + 1 : 1
 
+  const now = new Date()
+  const timestamp = Math.floor(now.getTime() / 1000)
   console.log("Sending data, iteration", num_iteration);
   await exporter.sendDataWithKey({
     timestamp: timestamp,
-    iso_date: new Date().toISOString(),
+    iso_date: now.toISOString(),
     key: key
   }, "key")
 
--- examples/send_dates_transaction.js
+++ examples/send_dates_transaction.js
@@ -3,19 +3,16 @@ const { Exporter } = require('../index')
 const exporter = new Exporter(pkg.name, true)
 
 async function pushData() {
-  const timestamp = Math.floor(new Date() / 1000)
-
   let lastPosition = await exporter.getLastPosition()
   console.log(`Last position: ${JSON.stringify(lastPosition)}`)
-  let key = 1
-  if(lastPosition) {
-    key = lastPosition.key + 1
-  }
+  const key = lastPosition ? lastPosition.key + 1 : 1
 
+  const now = new Date()
+  const timestamp = Math.floor(now.getTime() / 1000)
   console.log("Sending data with transaction...")
   await exporter.sendDataWithKey({
     timestamp: timestamp,
-    iso_date: new Date().toISOString(),
+    iso_date: now.toISOString(),
     key: key
   }, "key")
 
@@ -25,7 +22,6 @@ async function pushData() {
 
   let newPosition = await exporter.getLastPosition()
   console.log(`New position: ${JSON.stringify(newPosition)}`)
-
 }
 
 async function work() {
