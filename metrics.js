b2c67e9f8c23b6fe001d12522ed3ae5377260634
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2022-07-15 15:25:53 +0300

    wip

--- metrics.js
+++ metrics.js
@@ -1,52 +1,56 @@
-const {Counter, Gauge, Summary} = require('prom-client')
 const { logger } = require('./logger')
+const client = require('prom-client')
 
-module.exports.restartCounter = new Counter({
+module.exports.restartCounter = new client.Counter({
     name: 'restart_count',
     help: 'Number of restarts'
 });
 
-module.exports.downloadedTransactionsCounter = new Counter({
+module.exports.downloadedTransactionsCounter = new client.Counter({
     name: 'downloaded_transactions_count',
     help: 'Number of transactions downloaded'
 });
 
-module.exports.downloadedBlocksCounter = new Counter({
+module.exports.downloadedBlocksCounter = new client.Counter({
     name: 'downloaded_blocks_count',
     help: 'Number of blocks downloaded'
 });
 
-module.exports.requestsCounter = new Counter({
+module.exports.requestsCounter = new client.Counter({
     name: 'requests_made_count',
     help: 'Number of requests made to the node',
     labelNames: ['connection']
 });
 
-module.exports.requestsResponseTime = new Summary({
+module.exports.requestsResponseTime = new client.Summary({
     name: 'requests_response_time',
     help: 'The response time of the requests to the node',
     labelNames: ['connection']
 });
 
-module.exports.requestsQueueSize = new Gauge({
+module.exports.requestsQueueSize = new client.Gauge({
     name: 'requests_queue_size',
     help: 'Number of requests that are in the queue',
     labelNames: ['connection']
 });
 
-module.exports.lastExportedBlock = new Gauge({
+module.exports.lastExportedBlock = new client.Gauge({
     name: 'last_exported_block',
     help: 'The last block that was saved by the exporter'
 });
 
-module.exports.currentBlock = new Gauge({
+module.exports.currentBlock = new client.Gauge({
     name: 'current_block',
     help: 'The latest blocks on the blockchain'
 });
 
-module.exports.startCollection = function () {
+module.exports.startCollection = function (registry) {
     logger.info(`Starting the collection of metrics, the metrics are available on /metrics`);
-    require('prom-client').collectDefaultMetrics();
+    client.collectDefaultMetrics({ registry });
 };
 
-module.exports.register = require('prom-client').register
+module.exports.initRegistry = function () {
+    const Registry = client.Registry;
+    const register = new Registry();
+    return register
+};
